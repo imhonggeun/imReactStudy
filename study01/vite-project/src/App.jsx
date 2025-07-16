8fa30678d36511f9d1e9ebf75bd21b7499777c30
imhonggeun <dkfl9870@gmail.com>
2025-07-16 15:39:10 +0900

    블럭으로 벽 생성

--- study01/vite-project/src/App.jsx
+++ study01/vite-project/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import "./index.css"
 
 function App() {
-  
+  const start = 0;
+  const end = 4;
   const [model, setModel] = useState([])
-
+  const [point, setPoint] = useState([2, 0])
+  const [key, setKey] = useState(0)
   const model1 = [
     [1,0,0,0,1],
     [1,0,1,0,1],
@@ -12,28 +14,116 @@ function App() {
     [1,0,1,0,1],
     [1,0,0,0,1]
   ]
+  const model2 = [
+    [1,0,0,0,1],
+    [1,1,0,1,1],
+    [0,0,0,0,0],
+    [1,1,0,1,1],
+    [1,0,0,0,1]
+  ]
+  const model3 = [
+    [0,1,0,1,0],
+    [1,0,0,0,1],
+    [0,0,0,0,0],
+    [1,0,0,0,1],
+    [0,1,0,1,0]
+  ]
+  function reset() {
+    setPoint([2, 0])
+  }
   function btn1() {
+    reset()
     setModel(model1)
-  }  
+  }
+  function btn2() {
+    reset()
+    setModel(model2)
+  }
+  function btn3() {
+    reset()
+    setModel(model3)
+  }
+  useEffect(()=> {
+    if (model.length > 0) {
+      if(model[point[0]][point[1]] === 1) {
+        switch (key) {
+          case 37: // 왼쪽 이동
+            setPoint( [point[0], point[1] + 1] )
+            break;
+          case 38 : // 위쪽 이동
+            setPoint( [point[0] + 1, point[1]] )
+            break;
+          case 39 : // 오른쪽 이동
+            setPoint( [point[0], point[1] - 1] )
+            break;
+          case 40 : // 아래쪽 이동
+            setPoint( [point[0] - 1, point[1]] )
+            break;
+        }
+      }
+    }
+  }, [point])
+  useEffect(()=> {
+    function keyDown(e) {
+      // console.log(1, point)
+      setKey(e.keyCode)
+      switch (e.keyCode) {
+        case 37: // 왼쪽 이동
+          if(point[1] - 1 >= start) {
+            setPoint( [point[0], point[1] - 1] )
+          } else {
+            setPoint( [point[0], end] )
+          }
+          break;
+        case 38 : // 위쪽 이동
+          if(point[0] - 1 >= start) {
+            setPoint( [point[0] - 1, point[1]] )
+          } else {
+            setPoint( [end, point[1]] )
+          }
+          break;
+        case 39 : // 오른쪽 이동
+          if(point[1] + 1 <= end) {
+            setPoint( [point[0], point[1] + 1] )
+          } else {
+            setPoint( [point[0], start] )
+          }
+          break;
+        case 40 : // 아래쪽 이동
+          if(point[0] + 1 <= end) {
+            setPoint( [point[0] + 1, point[1]] )
+          } else {
+            setPoint( [start, point[1]] )
+          }
+          break;
+      }
+      // console.log(2, point)
+    }
+    window.addEventListener('keydown', keyDown)
+    return () => {
+      window.removeEventListener('keydown', keyDown)
+    }
+  }, [point])
 
   return (
     <main>
       <nav>
         <button type="button" id="1" onClick={btn1}>호출1</button>
-        <button type="button" id="2">호출2</button>
-        <button type="button">호출3</button>
+        <button type="button" id="2" onClick={btn2}>호출2</button>
+        <button type="button" onClick={btn3}>호출3</button>
       </nav>
       <section>
         <ul>
           { 
-            model?.map((li,index) => {
+            model?.map((li,y) => {
             return (
-                <li key={index}>
+                <li key={y}>
                   { 
-                    li?.map((div, i) => {
-                      const bg = (div === 1) ? 'bg1' : 'bg2'
+                    li?.map((div, x) => {
+                      let bg = (div === 1) ? 'bg1' : 'bg2'
+                      bg = (point[0] === y && point[1] === x) ? 'bg3' : bg;
                       return (
-                        <div className={bg} key={i}></div>
+                        <div className={bg} key={x}></div>
                       )
                     })
                   }
