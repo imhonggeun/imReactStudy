8a7f7adf77b9bba9c549c7b7807609e4c7add4fe
imhonggeun <dkfl9870@gmail.com>
2025-07-16 15:17:29 +0900

    전에 내용 그대로 복사

--- study01/vite-project/src/App.jsx
+++ study01/vite-project/src/App.jsx
@@ -1,9 +1,10 @@
-import { userState } from "react" //외부가져오기
+import { useState } from "react"
 import "./index.css"
-function App() {
 
-  const [model,setModel] = userState([])
+function App() {
   
+  const [model, setModel] = useState([])
+
   const model1 = [
     [1,0,0,0,1],
     [1,0,1,0,1],
@@ -11,36 +12,37 @@ function App() {
     [1,0,1,0,1],
     [1,0,0,0,1]
   ]
-  function btn1(){
+  function btn1() {
     setModel(model1)
-  }
+  }  
 
   return (
     <main>
-        <nav>
-            <button type="button" id="2">호출2</button>
-            <button type="button">호출3</button>
-        </nav>
-        <section>
-          <ul>
-            {
-              model?.map((li,index) => {
-                return (
-                  <li key={index}>
-                    {
-                      li?.map((div, i)=> {
-                        const bg =(div === 1 ) ? 'bg1' : 'bg2'
-                        return (
-                          <div classname={bg} key={i}></div>
-                        )
-                      })
-                    }
-                  </li>
-                )
-              })
-            }
-          </ul>
-        </section>
+      <nav>
+        <button type="button" id="1" onClick={btn1}>호출1</button>
+        <button type="button" id="2">호출2</button>
+        <button type="button">호출3</button>
+      </nav>
+      <section>
+        <ul>
+          { 
+            model?.map((li,index) => {
+            return (
+                <li key={index}>
+                  { 
+                    li?.map((div, i) => {
+                      const bg = (div === 1) ? 'bg1' : 'bg2'
+                      return (
+                        <div className={bg} key={i}></div>
+                      )
+                    })
+                  }
+                </li>
+              )
+            })            
+          }
+        </ul>
+      </section>
     </main>
   )
 }
