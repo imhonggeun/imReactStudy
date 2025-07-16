44747a10238151ddbd0a9df18ffd7f25bd271140
imhonggeun <dkfl9870@gmail.com>
2025-07-16 15:12:31 +0900

    useState 이용하여 화면 출력 하기

--- study01/vite-project/src/App.jsx
+++ study01/vite-project/src/App.jsx
@@ -1,31 +1,45 @@
+import { userState } from "react" //외부가져오기
 import "./index.css"
 function App() {
-  console.log(1234)
+
+  const [model,setModel] = userState([])
+  
+  const model1 = [
+    [1,0,0,0,1],
+    [1,0,1,0,1],
+    [0,0,0,0,0],
+    [1,0,1,0,1],
+    [1,0,0,0,1]
+  ]
+  function btn1(){
+    setModel(model1)
+  }
+
   return (
     <main>
         <nav>
-            <button type="button" id="1">호출1</button>
             <button type="button" id="2">호출2</button>
             <button type="button">호출3</button>
         </nav>
         <section>
-            <ul>
-                <li>
-                    <div class="bg2"></div><div class="bg1"></div><div class="bg2"></div><div class="bg1"></div><div class="bg2"></div>
-                </li>
-                <li>
-                    <div class="bg1"></div><div class="bg2"></div><div class="bg1"></div><div class="bg2"></div><div class="bg1"></div>
-                </li>
-                <li>
-                    <div class="bg2"></div><div class="bg1"></div><div class="bg2"></div><div class="bg1"></div><div class="bg2"></div>
-                </li>
-                <li>
-                    <div class="bg1"></div><div class="bg2"></div><div class="bg1"></div><div class="bg2"></div><div class="bg1"></div>
-                </li>
-                <li>
-                    <div class="bg2"></div><div class="bg1"></div><div class="bg2"></div><div class="bg1"></div><div class="bg2"></div>
-                </li>
-            </ul>
+          <ul>
+            {
+              model?.map((li,index) => {
+                return (
+                  <li key={index}>
+                    {
+                      li?.map((div, i)=> {
+                        const bg =(div === 1 ) ? 'bg1' : 'bg2'
+                        return (
+                          <div classname={bg} key={i}></div>
+                        )
+                      })
+                    }
+                  </li>
+                )
+              })
+            }
+          </ul>
         </section>
     </main>
   )
