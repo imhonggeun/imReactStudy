99006bb9f031403d5356612372c0b974232e9926
imhonggeun <dkfl9870@gmail.com>
2025-07-16 14:56:11 +0900

    오전에 수업 내용 가져오기

--- study01/vite-project/src/App.jsx
+++ study01/vite-project/src/App.jsx
@@ -1,12 +1,33 @@
-
+import "./index.css"
 function App() {
-  const [count, setCount] = useState(0)
-
+  console.log(1234)
   return (
-    <>
-    
-    
-    </>
+    <main>
+        <nav>
+            <button type="button" id="1">호출1</button>
+            <button type="button" id="2">호출2</button>
+            <button type="button">호출3</button>
+        </nav>
+        <section>
+            <ul>
+                <li>
+                    <div class="bg2"></div><div class="bg1"></div><div class="bg2"></div><div class="bg1"></div><div class="bg2"></div>
+                </li>
+                <li>
+                    <div class="bg1"></div><div class="bg2"></div><div class="bg1"></div><div class="bg2"></div><div class="bg1"></div>
+                </li>
+                <li>
+                    <div class="bg2"></div><div class="bg1"></div><div class="bg2"></div><div class="bg1"></div><div class="bg2"></div>
+                </li>
+                <li>
+                    <div class="bg1"></div><div class="bg2"></div><div class="bg1"></div><div class="bg2"></div><div class="bg1"></div>
+                </li>
+                <li>
+                    <div class="bg2"></div><div class="bg1"></div><div class="bg2"></div><div class="bg1"></div><div class="bg2"></div>
+                </li>
+            </ul>
+        </section>
+    </main>
   )
 }
 
--- study01/vite-project/src/main.jsx
+++ study01/vite-project/src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-
+//처음시작하는코드 render 할때 console 출력
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  //<StrictMode>
     <App />
-  </StrictMode>,
+  //</StrictMode>,
 )
