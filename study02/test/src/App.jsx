35cfcfb39fffef2727f986d777e2875dcc5469c9
imhonggeun <dkfl9870@gmail.com>
2025-07-17 10:57:38 +0900

    useState 알아보기

--- study02/test/src/App.jsx
+++ study02/test/src/App.jsx
@@ -1,34 +1,89 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const Test1 = () => {
+  const [state, setState] = useState(0)
+  //console.log('useState', useState(0))
+  const btn = () =>{ setState(state + 1) }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button type = 'button' onClick={btn}>버튼</button>
+      <h1>{state}</h1>
     </>
+  )
+}
+
+const Test2 = () => {
+  const [txt, setTxt] = useState('')
+  const submitEvent = (e) => {
+    e.preventDefault();
+    console.log(e.target.txt.value)
+    setTxt(e.target.txt.value)
+  }
+  return(
+    <form onSubmit={submitEvent}>
+      <input type='text' name='txt'/>
+      <button type='submit'>전송</button>
+      <h1>{txt}</h1>
+    </form>
+  )
+}
+
+const Test3 = () =>{
+  const [list,setlist] = useState([])
+  const [txt, setTxt] = useState('')
+  const submitEvent = (e) => {
+    e.preventDefault();
+    setlist([txt,...list])
+  }
+
+  const changeEvent = (e) => {
+    setTxt(e.target.value)
+  }
+
+  return(
+    <form onSubmit={submitEvent}>
+      <input type='text' name='txt' onChange={changeEvent}/>
+      <button>전송</button>
+      {
+        list.map((txt, index) => <p key={index}>{txt}</p>)
+      }
+      {/* <h1>{txt}</h1> */}
+    </form>
+  )
+}
+
+const Test4 = () =>{
+  const [list,setlist] = useState([])
+  const [txt, setTxt] = useState('')
+  const submitEvent = (e) => {
+    e.preventDefault();
+    setlist([txt,...list])
+  }
+
+  const changeEvent = (e) => {
+    setTxt(e.target.value)
+  }
+
+  return(
+    <form onSubmit={submitEvent}>
+      <input type='text' name='txt' onChange={changeEvent}/>
+      <button>전송</button>
+      {
+        list.map((txt, index) => <p key={index}>{txt}</p>)
+      }
+      {/* <h1>{txt}</h1> */}
+    </form>
+  )
+}
+
+//호출
+const App = () => {
+  return(
+    //<></>
+    //<Test1></Test1>
+    //<Test2></Test2>
+    <Test3></Test3>
   )
 }
 
