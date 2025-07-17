8914dfc00a2617b66c302fa1221f6e742517b490
imhonggeun <dkfl9870@gmail.com>
2025-07-17 13:53:15 +0900

    활용예제

--- study02/test/src/App3.jsx
+++ study02/test/src/App3.jsx
@@ -1,19 +1,48 @@
 import { useState,useEffect }  from "react"
 
-const App3Sub = () => {
+const App3Sub = ({txt, fnEvent}) => {
     return(
         <>
-            <h3>자식 영역</h3>
+            <h3>자식1 영역</h3>
+            <input type='text'value={txt} onChange={e => fnEvent(e.target.value)}/>
+        </>
+    )
+
+}
+
+const App3Sub2 = () =>{
+    const [cnt,setcnt] =useState(0)
+    useEffect(() => {
+        console.log("useEffect")
+        setcnt(cnt + 1)
+    },[])
+    return(
+        <>
+            <h3>자식2 영역</h3>
+            <p>{cnt}</p>
         </>
     )
 
 }
 
 const App3 = () => {
+    const[txt, txtset] = useState('')
+    const[show, isshow] = useState(true)
+
     return(
         <>
-            <h1>부모 영역</h1>
-            <App3Sub /> 
+
+            <h1>부모 영역 :{txt}</h1>
+            <button type="button" onClick={
+                () => isshow(!show)
+                }>버튼</button>
+            {
+                show && <App3Sub fnEvent={txtset} txt={txt}/>     
+            }
+            {
+                !show && <App3Sub2 />
+            }
+            
         </>
     )
 
