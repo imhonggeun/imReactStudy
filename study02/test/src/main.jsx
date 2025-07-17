7a80025db3fdfb971603a87c002804237e66db44
imhonggeun <dkfl9870@gmail.com>
2025-07-17 09:34:57 +0900

    react 생성 및 설치

--- study02/test/src/main.jsx
+++ study02/test/src/main.jsx
@@ -1,9 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './index.css'
 import App from './App.jsx'
-//처음시작하는코드 render 할때 console 출력
+
 createRoot(document.getElementById('root')).render(
-  //<StrictMode>
+  <StrictMode>
     <App />
-  //</StrictMode>,
+  </StrictMode>,
 )
