6fd815a0961978dbd54da8b4c4df9197f329ae7f
imhonggeun <dkfl9870@gmail.com>
2025-07-17 12:05:11 +0900

    useEffect 에서 useState 사용하기

---
+++ study02/test/src/App2.jsx
@@ -0,0 +1,38 @@
+import {useState, useEffect} from 'react';
+
+const Test1 = () => {
+  const [cnt, setCnt] = useState(0)
+  console.log(1)
+  useEffect(() => {
+    console.log(2)
+    setCnt(cnt + 1)
+  },[])
+  return (
+    <p>호출 횟수 : {cnt}</p>
+  )
+}
+
+const Test2 = () => {
+  const [list,setList] = useState([])
+  const [txt, setTxt] = useState('')
+  useEffect(() => {
+    console.log("useEffect",txt)
+    setList([txt,...list])
+  },[txt])
+  return(
+    <>
+      <input type='text' name='txt' 
+        value={txt}
+        onChange = { e => setTxt(e.target.value)}/>
+        {
+            list.map((txt, index) => <p key={index}>{txt}</p>)
+        }
+    </>
+  )
+}
+
+
+
+const App2 = () => (<Test2 />)
+
+export default App2
