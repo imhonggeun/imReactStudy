7454f628adcd410f38cef1a9938934eec895c05e
imhonggeun <dkfl9870@gmail.com>
2025-07-18 12:01:58 +0900

    useReducer 알아보기

--- study03/src/pages/UseReducer.jsx
+++ study03/src/pages/UseReducer.jsx
@@ -1,17 +1,41 @@
 import { useState, useReducer } from "react"
 import { Title } from '@pages/Home.jsx'
 
+
+const reducer = (state, action) =>{
+  switch(action.type) {
+      case '+':
+        state = state + action.value
+        break;
+      case '-':
+        state = state - action.value
+        break;
+      case '*':
+        state = state * action.value
+        break;
+      case '/':
+        state = state / action.value
+        break;
+  }
+  return state
+}
+
+
+
 const UseReducer = () => {
-  const [result, setResult] = useState(0)
+  //const [result, setResult] = useState(0)
+  const [state,dispatch] = useReducer(reducer,0);
   const [num, setNum] = useState(0)
   return (
     <>
       <Title title="useReducer" />
       <div style={{display: 'grid', gap: '5px', gridTemplateColumns: '1fr auto 1fr 1fr', marginTop: '10px'}}>
-        <span style={{textAlign: 'left'}}>{result}</span>
-        <span style={{textAlign: 'center'}}>+</span>
+        <span style={{textAlign: 'left'}}>{state}</span>
         <input type="number" style={{width: '100%'}} step="100" value={num} onChange={(e) => setNum(Number(e.target.value))} />
-        <button type="button" onClick={() => setResult(result + num)}>=</button>
+        <button type="button" onClick={() => dispatch({type: '+', value: num})}>+</button>
+        <button type="button" onClick={() => dispatch({type: '-', value: num})}>-</button>
+        <button type="button" onClick={() => dispatch({type: '*', value: num})}>*</button>
+        <button type="button" onClick={() => dispatch({type: '/', value: num})}>/</button>
       </div>
     </>
   )
